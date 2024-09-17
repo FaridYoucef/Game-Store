77a4ae01b7e20d1ff790bae81332b17a402fa439
FaridYoucef <songortekinn@gmail.com>
2024-09-17 21:49:52 +0100

    -Add serializers, views and URLs for porduct API -Include porduct URLs in the main porject URLs

---
+++ products/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from .models import Product, Category , Brand
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id', 'name', 'description', 'slug']
+        
+class BrandSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Brand
+        fields = ['id', 'name', 'website']
+        
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = [
+            'id', 'name', 'description', 'slug', 'price', 'brand' 'available',
+             'created', 'updated', 'image' 
+        ]
--- products/views.py
+++ products/views.py
@@ -1,11 +1,26 @@
 from django.shortcuts import render
-from .models import Category, Product
+from rest_framework import generics
+from .models import Category, Product, Brand
+from .serializers import ProductSerializer, CategorySerializer, BrandSerializer
 from django.http import HttpResponse
 
 # Create your views here.
-def product_list(request, category_slug):
-    catergory = None
-    products = Product.objects.filter(available)
+class productListView(generics.ListAPIView):
+    queryset = Product.objects.filter(available=True)
+    serializer_class = ProductSerializer
+    
+class ProductDetailView(generics.RetrieveAPIView):
+    queryset = Product.objects.filter(available=True)
+    serializer = ProductSerializer
+    lookup_field = 'slug'
+    
+class CategoryListView(generics.ListAPIView):
+    querySet = Category.objects.all()
+    serializer_class = CategorySerializer
+    
+class BrandListView(generics.ListAPIView):
+    queryset = Brand.objects.all()
+    serializer_class = BrandSerializer
     
 def home(request):
     return HttpResponse("<h1>Welcome to the Game Store!</h1>")

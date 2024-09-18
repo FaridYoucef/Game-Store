c26efeff8090fd8c328b54d10d46dc4cc74a3a07
FaridYoucef <songortekinn@gmail.com>
2024-09-18 02:09:02 +0100

    -Installing Postgresql and setting the Database -Connecting Postgresql and Django using psycopg2

--- gamingStore/settings.py
+++ gamingStore/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'gamingStore.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'gaming_store',
+        'USER': 'game',
+        'PASSWORD': 'Gamegamegame123456789',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 

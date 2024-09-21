1acb219d290d09bbe9a0069fad8aebd7908489d3
FaridYoucef <songortekinn@gmail.com>
2024-09-21 17:21:42 +0100

    -Updating the register mothod and serializrer: -Add the rest_framework Token -Hash the password

--- user/serializers.py
+++ user/serializers.py
@@ -2,14 +2,7 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 
 class UserSerializer(serializers.ModelSerializer):
-    class Meta:
+    class Meta(object):
         model = User
-        fields = ['username', 'email', 'password']
-    
-    def create(self, validated_data):
-        user = User.objects.create_user(
-            username=validated_data['username'],
-            email=validated_data['email'],
-            password=validated_data['password']
-        )
-        return user
+        fields = ['id', 'username', 'email', 'password']
+   
--- user/views.py
+++ user/views.py
@@ -1,14 +1,25 @@
 from rest_framework import status
 from rest_framework.decorators import api_view
+from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
 from .serializers import UserSerializer
 
+#Creating the user gegister method
 @api_view(['POST'])
 def register(request):
     if request.method == 'POST':
         serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
+            user = User.objects.get(username=request.data['username'])
+            user.set_password(request.data['password'])
+            token = Token.objects.create(user=user)
+            return Response({"token": token.key, "user": serializer.data})
             return Response({"message": "User created successfully"}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+    

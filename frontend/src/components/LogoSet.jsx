02101dc29025933b57bab060897439bcf55937d3
FaridYoucef <songortekinn@gmail.com>
2024-10-06 22:26:18 +0100

    - Add the logo set UI -Add the logo set into the Home page -Style the logos

---
+++ frontend/src/components/LogoSet.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const LogoSet = () => {
+  return (
+    <div  className='flex justify-center items-center gap-8 p-4 mt-8'>
+        <div><a href=""><img src="../games.jpeg" alt="" className='bg-transparent w-[120px]'/ ></a></div>
+        <div><a href=""><img src="../logops.jpg" alt="" className='w-[120px]'/></a></div>
+        <div><a href=""><img src="../logoxbx.png" alt="" className='w-[120px]'/></a></div>
+        <div><a href=""><img src="../ninilogo.jpg" alt="" className='w-[120px]'/></a></div>
+        <div><a href=""><img src="../hdlogo.jpg" alt="" className='w-[120px]'/></a></div>
+    </div>
+  )
+}
+
+export default LogoSet
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import HeroSlider from '../components/HeroSlider'
+import LogoSet from '../components/LogoSet'
 
 const HomePage = () => {
   return (
     <div>
         <HeroSlider />
+        <LogoSet />
     </div>  
   )
 }

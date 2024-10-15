218aae41c76767e89ecabf589ffed436503c15d6
FaridYoucef <songortekinn@gmail.com>
2024-10-15 08:26:53 +0100

    -Add the register component -Design the  registration from

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,21 +5,24 @@
   import CategoryPage from "./pages/CategoryPage";
   import ProductDetails from "./pages/ProductDetails";
 import ProductList from "./pages/ProductList";
+import Register from "./components/auth/Register";
 
+function App() {
+  return (
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/products" element={<ProductList />} />
+          <Route path="/categories/:slug" element={<CategoryPage />} />
+          <Route path="/productDetails/" element={<ProductDetails />} /> 
+          <Route path="/register/" element={<Register />} /> 
 
-  function App() {
-    return (
-      <Router>
-        <div>
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/products" element={<ProductList />} />
-            <Route path="/categories/:slug" element={<CategoryPage />} />
-          </Routes>
-        </div>
-      </Router>
-    );
-  }
+        </Routes>
+      </div>
+    </Router>
+  );
+}
 
   export default App;
---
+++ frontend/src/components/auth/Register.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+
+function Register() {
+  return (
+    <div className="max-w-md w-full bg-white p-8 rounded shadow-md mb-8 mx-auto my-10">
+      <h2 className="text-2xl font-bold mb-6">Register</h2>
+      <form>
+        <div className="mb-4">
+          <label htmlFor="username" className="block text-gray-700">
+            Username
+          </label>
+          <input
+            type="text"
+            name="username"
+            className="w-full p-2 border border-gray-300 rounded"
+            required
+          />
+        </div>
+        <div className="mb-4">
+          <label htmlFor="email" className="block text-gray-700">
+            Email
+          </label>
+          <input
+            type="email"
+            name="email"
+            className="w-full p-2 border border-gray-300 rounded"
+            required
+          />
+        </div>
+        <div className="mb-4">
+          <label htmlFor="password" className="block text-gray-700">
+            Password
+          </label>
+          <input
+            type="password"
+            name="password"
+            className="w-full p-2 border border-gray-300 rounded"
+            required
+          />
+        </div>
+        <button
+          type="submit"
+          className="w-full bg-fuchsia-700 text-white p-2 rounded mt-4"
+        >
+          Register
+        </button>
+      </form>
+        <p className="text-red-500 mt-4"></p>
+    </div>
+  );
+}
+
+export default Register;
